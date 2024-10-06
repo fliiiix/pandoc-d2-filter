3043b6dd486cc8edf6b8207a9199c850cb84f406
fliiiix <hi@l33t.name>
2024-10-06 16:35:46 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,7 @@
+https://github.com/ram02z/d2-filter
+
+````
+```{.d2 pad=20}
+x -> y
+```
+````
---
+++ pandoc_d2_filter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+from pandocfilters import toJSONFilter, Para, Image
+from pandocfilters import get_filename4code, get_caption, get_extension
+
+def d2(key, value, format, meta):
+    if key == "CodeBlock":
+        [[ident, classes, keyvals], code] = value
+
+        if "d2" in classes:
+            caption, typef, keyvals = get_caption(keyvals)
+
+            filename = get_filename4code("d2", code)
+            filetype = "png"
+
+            src = filename + ".d2"
+            dest = filename + "." + filetype
+
+            #sys.stderr.write(f"{src=} {dest=} {code=}")
+
+            txt = code.encode(sys.getfilesystemencoding())
+            with open(src, "wb") as f:
+                f.write(txt)
+
+            subprocess.check_call(["d2"] + [src, dest])
+            sys.stderr.write("Created image " + dest + "\n")
+
+            return Para([Image([ident, [], keyvals], caption, [dest, typef])])
+
+def main():
+    toJSONFilter(d2)
+
+
+if __name__ == "__main__":
+    main()
