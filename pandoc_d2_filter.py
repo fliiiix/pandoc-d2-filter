8a6ae262c60beac81026a2ef9e6828e3ede22e19
fliiiix <hi@l33t.name>
2024-10-07 17:02:24 +0200

    Add basic theme support

--- pandoc_d2_filter.py
+++ pandoc_d2_filter.py
@@ -5,7 +5,7 @@ import sys
 import subprocess
 
 from pandocfilters import toJSONFilter, Para, Image
-from pandocfilters import get_filename4code, get_caption, get_extension
+from pandocfilters import get_filename4code, get_value, get_caption, get_extension
 
 D2_BIN = os.environ.get("D2_BIN", "d2")
 
@@ -19,16 +19,17 @@ def d2(key, value, format, meta):
             filename = get_filename4code("d2", code)
             filetype = "png"
 
+            theme = get_value(keyvals, "theme", 0)[0]
             src = filename + ".d2"
             dest = filename + "." + filetype
 
-            #sys.stderr.write(f"{src=} {dest=} {code=}")
+            #sys.stderr.write(f"{keyvals=}")
 
             txt = code.encode(sys.getfilesystemencoding())
             with open(src, "wb") as f:
                 f.write(txt)
 
-            subprocess.check_call([D2_BIN, src, dest])
+            subprocess.check_call([D2_BIN, f"--theme={theme}", src, dest])
             sys.stderr.write("Created image " + dest + "\n")
 
             return Para([Image([ident, [], keyvals], caption, [dest, typef])])
